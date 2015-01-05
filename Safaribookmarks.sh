4a17b86afc354193c55dabfe7dbbcd2e0540365c
mrmstevenson <mrmstevenson@gmail.com>
2015-01-05 13:09:57 +0000

    Author
    Author in header

--- Safaribookmarks.sh
+++ Safaribookmarks.sh
@@ -3,6 +3,7 @@
 #This simple script I wrote to import Mac Safari bookmarks one time only
 # on the new user creation. This is to help provision of bookmarks to new
 # users on Mac enterprises.
+# by Matt the mac consultant - mrmstevenson@gmail.com 
 
 FILE=~/Library/Safari/Bookmarks.plist
 if [ ! -f $FILE ]
