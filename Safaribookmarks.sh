16351083f2aee339212fdc588a418be8fd9e41fa
mrmstevenson <mrmstevenson@gmail.com>
2015-01-05 13:08:39 +0000

    desciption
    description in header of script

--- Safaribookmarks.sh
+++ Safaribookmarks.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+#This simple script I wrote to import Mac Safari bookmarks one time only
+# on the new user creation. This is to help provision of bookmarks to new
+# users on Mac enterprises.
 
 FILE=~/Library/Safari/Bookmarks.plist
 if [ ! -f $FILE ]
