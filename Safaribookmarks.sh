88e4ebdde15f5aa2d339abade3e9473153090781
Matt Stevenson <mrmstevenson@gmail.com>
2015-01-05 13:50:43 +0000

    Update Safaribookmarks.sh

--- Safaribookmarks.sh
+++ Safaribookmarks.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #This simple script I wrote to import Mac Safari bookmarks one time only
-# on the new user creation. Please copy the corporate file to /tmp/Bookmarks.plist.
+# on the new user creation. The scirpt assumes the location of the corporate file to import is /tmp/Bookmarks.plist.
 # This is to help provision of bookmarks to new
 # users on Mac enterprises.
 # by Matt the mac consultant - mrmstevenson@gmail.com 
